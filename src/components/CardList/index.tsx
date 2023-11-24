4c75e0146a79ce0c70fe3318b6d0f4380792b643
whcender <heilender31@gmail.com>
2023-11-24 17:46:44 +0300

    next deploy

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -34,7 +34,7 @@ const Index: React.FC<PaginationProps> = async ({ page, cat }) => {
 
   return (
     <div className='md:flex-5'>
-      <h1 className='text-3xl font-bold max-md:2xl'>Popular Categories</h1>
+      <h1 className='text-3xl font-bold max-md:2xl'>Popular Kategoriler</h1>
       <div className='flex flex-col gap-11 mt-11'>
         {
           posts?.map((item: any) => (
