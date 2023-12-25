234b36832e539631ff18ee363db9766191854888
whcender <heilender31@gmail.com>
2023-12-25 18:03:51 +0300

    orospu cocğuyu

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,20 +1,15 @@
-import GitHubProvider from "next-auth/providers/github";
-import GooglePrivder from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import  prisma  from "@/utils/connect";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/utils/connect";
 import { getServerSession } from "next-auth";
 
 const options = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string
-    }),
-    GooglePrivder({
+    GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string
-    })
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
   ],
   pages: {
     signIn: '/login',
