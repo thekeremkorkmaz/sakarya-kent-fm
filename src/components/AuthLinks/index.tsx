06a869367408b81bf396b64fbb13a4bfc0c1e48b
whcender <heilender31@gmail.com>
2023-12-25 19:38:27 +0300

    menu fix v3

--- src/components/AuthLinks/index.tsx
+++ src/components/AuthLinks/index.tsx
@@ -1,48 +1,50 @@
-"use client";
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import { useState } from 'react';
-
-import { GiHamburgerMenu } from 'react-icons/gi'
+import { GiHamburgerMenu } from 'react-icons/gi';
 import { signOut, useSession } from 'next-auth/react';
-
 import HamburgerMenu from '@/components/HamburgerMenu';
 
 const Index = () => {
-  
   const { status } = useSession();
   const [showMenu, setShowMenu] = useState(false);
+
+  // Linklere tıklanınca menüyü kapat
+  const closeMenu = () => {
+    setShowMenu(false);
+  };
+
 
   return (
     <div>
       <div className='max-md:hidden'>
         {status === 'unauthenticated' ? (
           <>
-            <Link onClick={() => {
-              setShowMenu(false);
-            }} href="/login">Login</Link>
+            <Link href="/login">
+              <a onClick={closeMenu}>Login</a>
+            </Link>
           </>
         ) : (
           <div className='flex gap-3'>
-            <Link onClick={() => {
-              setShowMenu(false);
-            }} href="/write">Write</Link>
-            <span onClick={()=>{
-              signOut()
-              setShowMenu(false)
-            }} className='cursor-pointer'>Logout</span>
+            <Link href="/write">
+              <a onClick={closeMenu}>Write</a>
+            </Link>
+            <span
+              onClick={() => {
+                signOut();
+                closeMenu();
+              }}
+              className='cursor-pointer'
+            >
+              Logout
+            </span>
           </div>
         )}
-
       </div>
       <div className='hidden max-md:flex cursor-pointer'>
-        <GiHamburgerMenu size={25} onClick={() => {
-          setShowMenu(!showMenu);
-        }}/>
+        <GiHamburgerMenu size={25} onClick={() => setShowMenu(!showMenu)} />
         {showMenu && <HamburgerMenu />}
       </div>
     </div>
-
   );
 };
 
