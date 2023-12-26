aa4a92efa9b933042364960df66fa8ca0cf9b980
whcender <heilender31@gmail.com>
2023-12-26 23:50:20 +0300

    bu ne yav

--- src/components/SingleBlog/index.tsx
+++ src/components/SingleBlog/index.tsx
@@ -23,7 +23,7 @@ const index = ({ data, slug, keyVal }: Props) => {
 
   return (
     <div className='flex-5'>
-      <div key={keyVal} className='flex flex-col gap-3 max-md:text-sm w-[300px]' dangerouslySetInnerHTML={{ __html: htmlContent }} />
+      <p className='w-36'>{data}</p>
       <Comments postSlug={slug}/>
     </div>
   );
