0cffe55e8af8cb87e76ae8650eee43f7942126e6
whcender <heilender31@gmail.com>
2023-12-26 22:23:56 +0300

    la düzel aq

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -17,6 +17,7 @@ const formatCreatedAt = (createdAt: string): string => {
 const index = ({ item }: any) => {
 
   const formattedDate = formatCreatedAt(item.createdAt);
+  const textContent = typeof item.desc === 'string' ? item.desc.replace(/<[^>]*>/g, '') : '';
 
   return (
     <div key={item.id} className='flex items-center gap-4 max-md:gap-3 max-md:flex-col max-md:items-start'>
@@ -26,7 +27,7 @@ const index = ({ item }: any) => {
       <div className='md:flex-1 max-md:gap-1 flex flex-col md:h-[300px] justify-around'>
         <span className='opacity-80 max-md:text-xs'>{formattedDate}<b className='text-red-600 font-medium ml-2'>- {item.slug}</b></span>
         <h2 className='text-2xl max-md:text-lg font-semibold'>{item.title}</h2>
-        <p className='opacity-80 max-xl:text-sm max-md:text-xs max-w-52'>{`${item.desc.substring(0, 100)}...`}</p>
+        <p className='opacity-80 max-xl:text-sm max-md:text-xs w-52'>{`${textContent.substring(0, 100)}...`}</p>
         <Link className='w-max font-semibold max-md:text-xs' href={`/product/${item.slug}`}>Read More <div className='h-[1px] w-full bg-red-600'></div></Link>
       </div>
     </div>
