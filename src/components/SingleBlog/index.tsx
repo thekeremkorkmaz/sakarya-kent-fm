aa5e3bc3cd32261688998201cc41c1cd4433fe30
whcender <heilender31@gmail.com>
2023-12-27 00:59:55 +0300

    düzel nolur

--- src/components/SingleBlog/index.tsx
+++ src/components/SingleBlog/index.tsx
@@ -7,7 +7,8 @@ interface Props {
   keyVal: string;
 }
 
-const Index: React.FC<Props> = ({ data, slug, keyVal }: Props) => {
+const index = ({ data, slug, keyVal }: Props) => {
+
   if (!data) {
     return (
       <div className='flex-5'>
@@ -15,37 +16,17 @@ const Index: React.FC<Props> = ({ data, slug, keyVal }: Props) => {
           <p className='text-2xl font-semibold max-md:text-lg'>Opppsss birşeyler yanlış gitti... </p>
         </div>
       </div>
-    );
+    ) 
   }
 
-  const formatText = (text: string): string => {
-    const maxLength = 40;
-    const words = text.split(/\s+/); // Boşluklara göre kelimeleri ayır
-
-    let currentLine = '';
-    const formattedText = words.reduce((result: string[], word: string) => {
-      if ((currentLine + ' ' + word).length <= maxLength) {
-        currentLine += ' ' + word;
-      } else {
-        result.push(currentLine.trim());
-        currentLine = word;
-      }
-      return result;
-    }, []);
-
-    formattedText.push(currentLine.trim());
-
-    return formattedText.join('<br />');
-  };
-
-  const formattedData = formatText(data);
+  const htmlContent = typeof data === 'string' ? data : '';
 
   return (
     <div className='flex-5'>
-      <div key={keyVal} className='flex flex-col gap-3 max-md:text-sm w-[300px]' dangerouslySetInnerHTML={{ __html: formattedData }} />
-      <Comments postSlug={slug} />
+      <div key={keyVal} className='flex flex-col gap-3 max-md:text-sm w-[300px]' dangerouslySetInnerHTML={{ __html: htmlContent }} />
+      <Comments postSlug={slug}/>
     </div>
   );
 };
 
-export default Index;
+export default index;
