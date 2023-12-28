176565e8fd0aeab37029bb7e6619856339997b4a
whcender <heilender31@gmail.com>
2023-12-28 14:13:39 +0300

    only google

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -22,9 +22,9 @@ const page = () => {
         <div className='bg-gray-300 py-28 px-32 flex flex-col gap-9 text-xl max-sm:text-base text-white rounded-lg max-sm:py-24 max-sm:px-16'>
             <div onClick={()=>{
               signIn('google')
-            }} className='py-4 cursor-pointer px-6 max-sm:px-4 rounded-lg bg-red-500'>Sign in with Google</div>
-            <div className='py-4 cursor-pointer px-6 max-sm:px-4 rounded-lg bg-black'>Sign in with Github</div>
-            <div  className='py-4 cursor-pointer px-6 max-sm:px-4 rounded-lg bg-blue-500'>Sign in with Facebook</div>
+            }} className='py-4 cursor-pointer px-6 max-sm:px-4 rounded-lg bg-red-500 '>Sign in with Google</div>
+            <div className='py-4 px-6 max-sm:px-4 rounded-lg bg-black cursor-not-allowed'>Sign in with Github</div>
+            <div  className='py-4 px-6 max-sm:px-4 rounded-lg bg-blue-500 cursor-not-allowed'>Sign in with Facebook</div>
         </div>
     </div>
   )
