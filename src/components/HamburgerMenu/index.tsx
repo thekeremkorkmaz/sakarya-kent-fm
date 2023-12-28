14786625f0c373b9bdacbcca19761953ba3b6d50
whcender <heilender31@gmail.com>
2023-12-28 03:46:22 +0300

    hamburger menuyede devre dışı bıraktık

--- src/components/HamburgerMenu/index.tsx
+++ src/components/HamburgerMenu/index.tsx
@@ -1,16 +1,16 @@
 "use client";
-import React from 'react'
-import Link from 'next/link'
-import { useGenerationStore } from '@/store/idea-generation'
+import React from 'react';
+import Link from 'next/link';
+import { useGenerationStore } from '@/store/idea-generation';
 import { AuthLinksProps } from '@/types/index';
 import { useSession, signOut } from 'next-auth/react';
 
+const Index = () => {
+  const { status, data } = useSession();
+  const { showMenu, setShowMenu } = useGenerationStore();
 
-const index = () => {
-
-  const { status } = useSession();
-
-  const { showMenu, setShowMenu } = useGenerationStore()
+  // E-posta adresinin belirli bir formata uygun olup olmadığını kontrol et
+  const isValidEmail = data?.user?.email === 'azratekin400@gmail.com';
 
   return (
     <div className='flex items-center justify-center flex-col left-0 px-5 py-6 absolute h-[100%] bg-black text-white text-3xl w-full z-50 gap-10 top-16 '>
@@ -19,23 +19,31 @@ const index = () => {
       <Link href="/">About</Link>
       {status === 'unauthenticated' ? (
         <>
-          <Link onClick={() => {
-            setShowMenu(false)
-          }} href="/login">Login</Link>
+          <Link onClick={() => setShowMenu(false)} href="/login">
+            Login
+          </Link>
         </>
       ) : (
         <>
-          <Link onClick={() => {
-            setShowMenu(false)
-          }} href="/write">Write</Link>
-          <span onClick={() => {
-            setShowMenu(false)
-            signOut()
-          }} className='cursor-pointer'>Logout</span>
+          {/* Eğer e-posta uygunsa, "Write" linkini göster */}
+          {isValidEmail && (
+            <Link onClick={() => setShowMenu(false)} href="/write">
+              Write
+            </Link>
+          )}
+          <span
+            onClick={() => {
+              setShowMenu(false);
+              signOut();
+            }}
+            className='cursor-pointer'
+          >
+            Logout
+          </span>
         </>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default index;
+export default Index;
