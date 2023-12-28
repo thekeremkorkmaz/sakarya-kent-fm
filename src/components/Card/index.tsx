458e72da347f152706db46a553bbb1dcb692b1a0
whcender <heilender31@gmail.com>
2023-12-28 03:26:44 +0300

    card image

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -23,7 +23,7 @@ const index = ({ item }: any) => {
   return (
     <div key={item.id} className='flex items-center gap-4 max-md:gap-3 max-md:flex-col max-md:items-start'>
       <div className='md:flex-1 md:w-[500px] max-md:text-left max-md:w-full'>
-        <Image className='w-[400px] h-[300px] object-cover max-md:w-[150px] max-md:h-[150px] max-md:rounded-[50%]' src="/p1.jpeg" height={300} width={300} alt='cate' />
+        <Image className='w-[400px] h-[300px] object-cover max-md:w-[150px] max-md:h-[150px] max-md:rounded-[50%]' src={item.img ? item.img : `/p1.jpeg`} height={300} width={300} alt='cate' />
       </div>
       <div className='md:flex-1 max-md:gap-1 flex flex-col md:h-[300px] justify-around'>
         <span className='opacity-80 max-md:text-xs'>{formattedDate}<b className='text-red-600 font-medium ml-2'>- {item.slug}</b></span>
