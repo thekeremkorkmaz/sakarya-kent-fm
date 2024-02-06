2aa076707b3ba39a4205a503060211bbf24b16cf
whcender <heilender31@gmail.com>
2024-02-06 18:54:01 +0300

    buton position

--- src/components/goListen/index.tsx
+++ src/components/goListen/index.tsx
@@ -10,7 +10,7 @@ const Index = () => {
   };
 
   return (
-    <div>
+    <div className='flex justify-center items-center'>
       <button className='bg-black py-2 px-4 text-white rounded-lg text-center' onClick={handleScrollToBottom}>Radyomuzu Dinleyin!</button>
     </div>
   );
