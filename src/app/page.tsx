71478d0476b1f9422b3321902ad4473c5fb961c1
whcender <heilender31@gmail.com>
2024-02-06 18:48:02 +0300

    listen pag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Featured from '@/components/Featured'
 import CardList from '@/components/CardList'
 import Menu from '@/components/Menu'
 import Listen from '@/components/Listen'
+import GoListen from '@/components/goListen'
 
 export default function Home({searchParams} : any) {
   
@@ -9,6 +10,7 @@ export default function Home({searchParams} : any) {
 
   return (
     <div className='flex flex-col gap-28 mt-44 max-lg:mt-16 max-md:mt-11'>
+      <GoListen />
       <Listen />
       <Featured />
       <div className='flex gap-11 max-md:flex-col'>
