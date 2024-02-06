0ef7ea0ce4fd5b738f7038a7e1068d0e45890d79
whcender <heilender31@gmail.com>
2024-02-06 18:19:35 +0300

    helment try

--- src/components/Listen/index.tsx
+++ src/components/Listen/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Helmet } from "react-helmet";
 
 const index = () => {
   return (
-    <div>index</div>
+    <div>
+      <Helmet>
+        <script type='text/javascript'>
+          var cstrFreePlayerUid = 587459;var cstrFreePlayerTheme = 'color';var cstrFreePlayerColor = '000000';
+        </script>
+        <script type='text/javascript' src='//corscdn.caster.fm/freeplayer/FreePlanPlayerEmbed.js'></script>
+
+      </Helmet>
+    </div>
   )
 }
 
