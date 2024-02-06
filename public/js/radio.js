befea1ecb0d22cab48752d281d4e54d3a2b6cb23
whcender <heilender31@gmail.com>
2024-02-06 18:27:25 +0300

    new try

---
+++ public/js/radio.js
@@ -0,0 +1,8 @@
+var cstrFreePlayerUid = 587459;
+var cstrFreePlayerTheme = 'color';
+var cstrFreePlayerColor = '000000';
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = '//corscdn.caster.fm/freeplayer/FreePlanPlayerEmbed.js';
+document.head.appendChild(script);
--- src/components/Listen/index.tsx
+++ src/components/Listen/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Script from 'next/script'
 
 const index = () => {
   return (
     <div>
       <h1>yok</h1>
+      <Script src='/js/radio.js'/>
     </div>
   )
 }
