ba688c8fb8ac3e90dc42f8d171b1b39910a93f28
whcender <heilender31@gmail.com>
2024-02-06 18:16:06 +0300

    denee

--- src/components/Listen/index.tsx
+++ src/components/Listen/index.tsx
@@ -1,19 +1,9 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react'
 
-export default function Document() {
+const index = () => {
   return (
-    <Html>
-      <Head>
-        <script type='text/javascript'>
-          var cstrFreePlayerUid = 587459;var cstrFreePlayerTheme = 'color';var cstrFreePlayerColor = '000000';
-        </script>
-        <script type='text/javascript' src='//corscdn.caster.fm/freeplayer/FreePlanPlayerEmbed.js'></script>
-        
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <div>index</div>
   )
 }
+
+export default index
