c5ef4a9309d3dc8071a80bbb0fba32543932755f
whcender <heilender31@gmail.com>
2024-02-06 18:14:56 +0300

    radio

--- src/components/Listen/index.tsx
+++ src/components/Listen/index.tsx
@@ -1,24 +1,19 @@
-"use client";
-import React from 'react'
-import { FaPlay } from "react-icons/fa";
-import { FaStop } from "react-icons/fa";
+import { Html, Head, Main, NextScript } from 'next/document'
 
-
-
-const page = () => {
-
-  const [playing, setPlaying] = React.useState(false);
-
+export default function Document() {
   return (
-    <div className='flex justify-center flex-col gap-3 items-center'>
-      <script type="text/javascript">var cstrFreePlayerUid = 587459;var cstrFreePlayerTheme = "yellow";var cstrFreePlayerColor = "000000";</script>	<script type="text/javascript" src="//corscdn.caster.fm/freeplayer/FreePlanPlayerEmbed.js"></script>	<!--   DO NOT REMOVE THE LINKS BELOW, THEY  WILL BE HIDDEN (AND WILL HELP US A LOT)   -->	<a id="cstrFreePlayerBL1" href="//www.caster.fm/">Free Shoutcast Hosting</a><a id="cstrFreePlayerBL2" href="//www.caster.fm/">Radio Stream Hosting</a>	<div id="cstrFreePlayerDiv"></div>
-      {/* DO NOT REMOVE THE LINKS BELOW, THEY WILL BE HIDDEN (AND WILL HELP US A LOT) */}
-      <a id='cstrFreePlayerBL1' href='//www.caster.fm/'>Free Shoutcast Hosting</a>
-      <a id='cstrFreePlayerBL2' href='//www.caster.fm/'>Radio Stream Hosting</a>
-      <div id='cstrFreePlayerDiv'></div>
-
-    </div>
+    <Html>
+      <Head>
+        <script type='text/javascript'>
+          var cstrFreePlayerUid = 587459;var cstrFreePlayerTheme = 'color';var cstrFreePlayerColor = '000000';
+        </script>
+        <script type='text/javascript' src='//corscdn.caster.fm/freeplayer/FreePlanPlayerEmbed.js'></script>
+        
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
   )
 }
-
-export default page
