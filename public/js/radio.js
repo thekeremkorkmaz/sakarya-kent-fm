376a75ad1315bc7b8378c793ae9766a7d5dc52ed
whcender <heilender31@gmail.com>
2024-02-06 18:30:09 +0300

    linkler

--- public/js/radio.js
+++ public/js/radio.js
@@ -6,3 +6,19 @@ var script = document.createElement('script');
 script.type = 'text/javascript';
 script.src = '//corscdn.caster.fm/freeplayer/FreePlanPlayerEmbed.js';
 document.head.appendChild(script);
+
+var link1 = document.createElement('a');
+link1.id = 'cstrFreePlayerBL1';
+link1.href = '//www.caster.fm/';
+link1.textContent = 'Free Shoutcast Hosting';
+document.body.appendChild(link1);
+
+var link2 = document.createElement('a');
+link2.id = 'cstrFreePlayerBL2';
+link2.href = '//www.caster.fm/';
+link2.textContent = 'Radio Stream Hosting';
+document.body.appendChild(link2);
+
+var div = document.createElement('div');
+div.id = 'cstrFreePlayerDiv';
+document.body.appendChild(div);
