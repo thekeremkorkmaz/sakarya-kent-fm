11f00eccbdd646ea8ed296b855abb40bb553a81b
thekeremkorkmaz <167742361+thekeremkorkmaz@users.noreply.github.com>
2024-04-25 17:36:08 +0300

    Update index.tsx

--- src/components/Featured/index.tsx
+++ src/components/Featured/index.tsx
@@ -5,6 +5,7 @@ import {anabaslik} from '../../texts/index'
 import {anabaslik2} from '../../texts/index'
 import {ilkyazi} from '../../texts/index'
 import {butonyazi} from '../../texts/index'
+import {foto} from '../../texts/index'
 
 const index = () => {
   return (
@@ -12,7 +13,7 @@ const index = () => {
       <h1 className='md:text-5xl lg:text-7xl text-3xl leading-normal'>{anabaslik}</h1>
       <div className='mt-16 md:gap-11 flex justify-center items-center'>
         <div className='md:flex-1'>
-          <Image className='w-[500px] h-[500px] max-lg:w-[400px] max-lg:h-[400px]  max-md:hidden' src="/kent.png" height={500} width={400} alt='featured'/>
+          <img className='w-[500px] h-[500px] max-lg:w-[400px] max-lg:h-[400px]  max-md:hidden' src={foto} alt='featured'/>
         </div>
         <div className='md:flex-1 flex gap-8 flex-col text-left'>
           <h2 className='text-4xl font-semibold max-md:text-2xl max-lg:text-2xl'>{anabaslik2}</h2>
