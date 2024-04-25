9704b402c66ccd6f64c4c0f4d3b53e647feb8dd1
whcender <heilender31@gmail.com>
2024-04-25 13:17:46 +0300

    dash

--- src/components/Featured/index.tsx
+++ src/components/Featured/index.tsx
@@ -1,29 +1,23 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import {anabaslik} from '../../texts/index'
+import {anabaslik2} from '../../texts/index'
+import {ilkyazi} from '../../texts/index'
+import {butonyazi} from '../../texts/index'
 
 const index = () => {
   return (
     <div >
-      <h1 className='md:text-5xl lg:text-7xl text-3xl leading-normal'><b> Sakarya Kent FM;</b> Şehrin En Fiyakalı Radyosu</h1>
+      <h1 className='md:text-5xl lg:text-7xl text-3xl leading-normal'>{anabaslik}</h1>
       <div className='mt-16 md:gap-11 flex justify-center items-center'>
         <div className='md:flex-1'>
           <Image className='w-[500px] h-[500px] max-lg:w-[400px] max-lg:h-[400px]  max-md:hidden' src="/kent.png" height={500} width={400} alt='featured'/>
         </div>
         <div className='md:flex-1 flex gap-8 flex-col text-left'>
-          <h2 className='text-4xl font-semibold max-md:text-2xl max-lg:text-2xl'>Sakarya Kent FM: Yeni Bir Nefes, Yepyeni Bir Ritim!</h2>
-          <p className='text-md max-lg:text-sm opacity-75'>Şehrin En Fiyakalı Radyosu!
-
-Türkçe Rap, Rock, Pop ve Jazz severleri bir araya toplayan tek radyo !
-
-Sakarya'nın ritmini ve ruhunu yansıtan Sakarya Kent FM, şehrin en fiyakalı radyosu olarak kaliteli müzik, canlı yayınlar, radyo tiyatroları, sesli kitaplar, podcastler, talk showlar ile spordan sinemaya, borsadan tasavvufa, siyasetten felsefe ve bilime kadar pek çok program ile eşsiz bir deneyim sunuyor.
-
-Şehrin ve dinleyenlerin nabzını tutan , en yeni şarkılar, sevilen klasikler ve birbirinden ilginç konuklarla dolu yayın akışıyla Sakarya Kent FM; Sakarya'nın vazgeçilmez radyosu.
-
-
-SAKARYA KENT FM - GENEL YAYIN YÖNETMENİ:
-KEREM KORKMAZ</p>
-          <Link href={"/product/asdasdasdsad"} className='w-max px-4 py-3 max-md:px-3 max-md:py-2 border-2 bg-[#e5e5e5] text-black rounded-lg text-sm'>Read More</Link>
+          <h2 className='text-4xl font-semibold max-md:text-2xl max-lg:text-2xl'>{anabaslik2}</h2>
+          <p className='text-md max-lg:text-sm opacity-75'>{ilkyazi}</p>
+          <Link href={"/product/asdasdasdsad"} className='w-max px-4 py-3 max-md:px-3 max-md:py-2 border-2 bg-[#e5e5e5] text-black rounded-lg text-sm'>{butonyazi}</Link>
         </div>
       </div>
     </div>
---
+++ src/texts/index.ts
@@ -0,0 +1,21 @@
+// !!Sadece tırnak içindeki yerleri değiştirin. Diğer yerleri değiştirmeyin. Tırnak işaretlerini silmeyin.!!
+
+// sayfanın en başındaki başlık
+export const anabaslik = "Sakarya Kent FM; Şehrin En Fiyakalı Radyosu" 
+
+//fotoğraf yanındaki başlık
+export const anabaslik2 = "Sakarya Kent FM: Yeni Bir Nefes, Yepyeni Bir Ritim!"
+
+// fotoğrafın yanındaki yazı
+export const ilkyazi = "Şehrin En Fiyakalı Radyosu! Türkçe Rap, Rock, Pop ve Jazz severleri bir araya toplayan tek radyo ! Sakarya'nın ritmini ve ruhunu yansıtan Sakarya Kent FM, şehrin en fiyakalı radyosu olarak kaliteli müzik, canlı yayınlar, radyo tiyatroları, sesli kitaplar, podcastler, talk showlar ile spordan sinemaya, borsadan tasavvufa, siyasetten felsefe ve bilime kadar pek çok program ile eşsiz bir deneyim sunuyor. Şehrin ve dinleyenlerin nabzını tutan , en yeni şarkılar, sevilen klasikler ve birbirinden ilginç konuklarla dolu yayın akışıyla Sakarya Kent FM; Sakarya'nın vazgeçilmez radyosu. SAKARYA KENT FM - GENEL YAYIN YÖNETMENİ: KEREM KORKMAZ"
+
+//fotoğrafın yanındaki buton yazısı
+
+export const butonyazi = "Daha Fazla"
+
+
+// populer kategoriler yazısı
+export const blogbaslik = "Popular Kategoriler"
+
+//en alttaki açıklama yazısı
+export const footertext= "Şehrin En Fiyakalı Radyosu! Türkçe Rap, Rock, Pop ve Jazz severleri bir araya toplayan tek radyo ! Şehrin ve dinleyenlerin nabzını tutan , en yeni şarkılar, sevilen klasikler ve birbirinden ilginç konuklarla dolu yayın akışıyla Sakarya Kent FM; Sakarya'nın vazgeçilmez radyosu."
