10b7977e8fd410027ea9b6fca4d7df682b37e8c8
thekeremkorkmaz <167742361+thekeremkorkmaz@users.noreply.github.com>
2024-04-25 17:36:27 +0300

    Update index.ts

--- src/texts/index.ts
+++ src/texts/index.ts
@@ -9,7 +9,7 @@ export const anabaslik = "Sakarya Kent FM; Şehrin En Fiyakalı Radyosu"
 export const anabaslik2 = "Sakarya Kent FM: Yeni Bir Nefes, Yepyeni Bir Ritim!"
 
 //anasayfa fotoğrafı !!sadece resmin orjinal linkini girin hızlı resime yükleyebilirisniz.!!
-export const foto = ""
+export const foto = "https://i.hizliresim.com/79h2oip.jpg"
 
 // fotoğrafın yanındaki yazı
 export const ilkyazi = "Şehrin En Fiyakalı Radyosu! Türkçe Rap, Rock, Pop ve Jazz severleri bir araya toplayan tek radyo ! Sakarya'nın ritmini ve ruhunu yansıtan Sakarya Kent FM, şehrin en fiyakalı radyosu olarak kaliteli müzik, canlı yayınlar, radyo tiyatroları, sesli kitaplar, podcastler, talk showlar ile spordan sinemaya, borsadan tasavvufa, siyasetten felsefe ve bilime kadar pek çok program ile eşsiz bir deneyim sunuyor. Şehrin ve dinleyenlerin nabzını tutan , en yeni şarkılar, sevilen klasikler ve birbirinden ilginç konuklarla dolu yayın akışıyla Sakarya Kent FM; Sakarya'nın vazgeçilmez radyosu. SAKARYA KENT FM - GENEL YAYIN YÖNETMENİ: KEREM KORKMAZ"
